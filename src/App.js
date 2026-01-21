b1b20b94d7b14ec3f61a7722fa4d10349bb76c1c
Bhuvan.R <bhuvan.in>
2026-01-21 19:06:46 +0530

    Deploy gym frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Members from "./pages/Members";
+import AddMember from "./pages/AddMember";
+import Navbar from "./components/Navbar";
+import PrivateRoute from "./components/PrivateRoute";
+import Attendance from "./pages/Attendance";
+import MonthlyReport from "./pages/MonthlyReport";
+import ExpiringMembers from "./pages/ExpiringMembers";
+import MemberDetail from "./pages/MemberDetail"; // Import the new MemberDetail component
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/members"
+          element={
+            <PrivateRoute>
+              <Members />
+            </PrivateRoute>
+          }
+        />
+
+        {/* New Route for Member Detail Page */}
+        <Route
+          path="/members/:id"
+          element={
+            <PrivateRoute>
+              <MemberDetail />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/add-member"
+          element={
+            <PrivateRoute>
+              <AddMember />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+        path="/attendance"
+          element={
+            <PrivateRoute>
+              <Attendance />
+            </PrivateRoute>
+          }
+        />
+        <Route
+        path="/reports/monthly"
+        element={
+          <PrivateRoute>
+            <MonthlyReport />
+          </PrivateRoute>
+        }
+        />
+        <Route
+          path="/reports/expiring"
+          element={
+            <PrivateRoute>
+              <ExpiringMembers />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
